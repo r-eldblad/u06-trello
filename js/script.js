984f5588d319651e587523919f2d2552bc5a3eea
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-05-27 22:11:58 +0200

    This commit implements dialog and datepicker, as well as tabs

--- js/script.js
+++ js/script.js
@@ -4,4 +4,28 @@ $(document).ready(function () {
       connectWith: ".listOfCards",
     })
     .disableSelection();
+
+  $("#dialog").hide();
+
+  $(".card").click(function () {
+    $("#dialog")
+      .dialog({
+        width: 500,
+        closeOnEscape: true,
+        show: {
+          effect: "fade",
+          duration: 400,
+        },
+        hide: {
+          effect: "fade",
+          duration: 400,
+        },
+      })
+      .bind("clickoutside", function (event) {
+        $(this).hide();
+      });
+
+    $("#datepicker").datepicker();
+    $("#tabs").tabs();
+  });
 });
